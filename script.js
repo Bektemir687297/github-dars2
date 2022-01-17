2be57a8f9b8066b5fb8ea0ab76a339567b393013
Bek <bek.abduazimov@gmail.com>
2022-01-17 18:57:32 +0500

    Bek

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfNews = +prompt("Siz qancha yangliki ko'rdingiz?");
+let numberOfNews;
+
+function startProject() {
+  numberOfNews = +prompt("Siz qancha yangliki ko'rdingiz?");
+
+  while (numberOfNews == "" || numberOfNews == null || isNaN(numberOfNews)) {
+    numberOfNews = +prompt("Siz qancha yangliki ko'rdingiz?");
+  }
+}
+startProject();
+
 const personalNewsDB = {
   count: numberOfNews,
   news: {},
@@ -8,25 +18,48 @@ const personalNewsDB = {
   genres: [],
   privat: false,
 };
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Oxirgi ko'rgan yanglingizdan biri?"),
-    b = prompt("Unga qancha baho bergan bo'lardingiz?");
 
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalNewsDB.news[a] = b;
-    console.log("Tayyor");
-  } else {
-    console.log("Error");
-    i--;
+function rememberNews() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Oxirgi ko'rgan yanglingizdan biri?"),
+      b = prompt("Unga qancha baho bergan bo'lardingiz?");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalNewsDB.news[a] = b;
+      console.log("Tayyor");
+    } else {
+      console.log("Error");
+      i--;
+    }
   }
 }
-if (personalNewsDB.count < 10) {
-  console.log("Judda kam yangliklar o'qilibdi!");
-} else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
-  console.log("Siz klassik tomoshbinsiz!");
-} else if (personalNewsDB.count >= 30) {
-  console.log("Siz yangliklar ishqibozi ekansiz!");
-} else {
-  console.log("Xatolik yuz berdi");
+rememberNews();
+
+function showPasonalLeval() {
+  if (personalNewsDB.count < 10) {
+    console.log("Judda kam yangliklar o'qilibdi!");
+  } else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
+    console.log("Siz klassik tomoshbinsiz!");
+  } else if (personalNewsDB.count >= 30) {
+    console.log("Siz yangliklar ishqibozi ekansiz!");
+  } else {
+    console.log("Xatolik yuz berdi");
+  }
 }
-console.log(personalNewsDB);
+showPasonalLeval();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalNewsDB);
+  }
+}
+showMyDB(personalNewsDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalNewsDB.genres[i - 1] = prompt(
+      `Sizning sevmli janringi no'miri ${i}`
+    );
+  }
+}
+writeYourGenres();
